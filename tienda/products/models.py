c0f05e4f61dbab6aa9d5395685add885181833cb
Julia <julia.berning@thenotcompany.com>
2024-11-24 19:19:34 -0300

    feat: add category model

--- tienda/products/models.py
+++ tienda/products/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de categorías
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nombre')
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+# Modelo de productos
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Nombre')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='get_products', verbose_name='Categoría')
     description = models.TextField(verbose_name='Descripción')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio')
 
--- tienda/products/serializers.py
+++ tienda/products/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Category, Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price')
+        # fields = ('id', 'name', 'description', 'price') # Para incluir solo los campos especificados en orden seleccionado
+        fields = '__all__' # Para incluir todos los campos del modelo en orden de declaración
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
