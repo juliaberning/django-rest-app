437154ac3ed357103dc948956e0f640c230c86e5
Julia <julia.berning@thenotcompany.com>
2024-11-24 22:03:16 -0300

    feat: filter products by category

--- tienda/products/views.py
+++ tienda/products/views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
-
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # Filtrar productos por categoría
+    # http://127.0.0.1:8000/api/products/by_category/?Category=1
+    @action(detail=False)
+    def by_category(self, request):
+        category = self.request.query_params.get('Category', None)
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
