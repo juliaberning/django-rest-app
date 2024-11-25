e340c94bb400639e8a4302cda8e6369fd8f381b6
Julia <julia.berning@thenotcompany.com>
2024-11-24 21:14:46 -0300

    chore: use ViewSets and DefaultRouter

--- tienda/products/urls.py
+++ tienda/products/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import ProductList, ProductDetail
 
-urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-]
+from .views import CategoryViewSet, ProductViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('products', ProductViewSet,basename='products')
+
+urlpatterns = router.urls
+
+
+# Django REST Framework proporciona otro sistema para definir las rutas de la API, que es más simple y directo.
+# urlpatterns = [
+#     path('products/', ProductList.as_view(), name='product-list'),
+#     path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+# ]
--- tienda/products/views.py
+++ tienda/products/views.py
@@ -1,11 +1,21 @@
-from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import viewsets
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-class ProductList(generics.ListCreateAPIView):
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# Se puede usar un viewset (más flexible) en lugar de generics
+# class ProductList(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
