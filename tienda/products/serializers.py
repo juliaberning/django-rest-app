3579a79bd2d4707bdf2f752ed6e7580c84bb092b
Julia <julia.berning@thenotcompany.com>
2024-11-27 20:35:58 -0300

    fix: improve category and price descriptions by adding ReadOnlyFields to serializers

--- tienda/products/serializers.py
+++ tienda/products/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Category, Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    # Serializers no permiten acceder a campos de otros modelos directamente, 
+    # por lo que se hace una referencia a través de un campo de solo lectura
+    category_name = serializers.ReadOnlyField(source='category.name') 
+    price_type_description = serializers.ReadOnlyField(source='get_price_type_display')   
     class Meta:
         model = Product
         # fields = ('id', 'name', 'description', 'price') # Para incluir solo los campos especificados en orden seleccionado
--- vue-django/src/views/HomeView.vue
+++ vue-django/src/views/HomeView.vue
@@ -9,16 +9,13 @@
         />
         <div class="card-body">
           <h5 class="card-title">{{ product.name }}</h5>
-          <p class="card-text">
-            {{ product.category }}
-          </p>
+          <p class="card-text">Categoría: {{ product.category_name }}</p>
           <p class="card-text">
             {{ product.description }}
           </p>
           <p class="card-text">
-            ${{ product.price }} ({{ product.price_type }})
+            ${{ product.price }} ({{ product.price_type_description }})
           </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
     </div>
